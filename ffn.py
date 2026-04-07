ab6d8c36a80d8830834fae80046d07a136092aae
minato-elgato <samadhmd83@gmail.com>
2026-04-07 16:17:36 +0530

    feat: added SwiGLU FeedForward network

---
+++ ffn.py
@@ -0,0 +1,25 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class FeedForward(nn.Module):
+    def __init__(self, d_in: int, hidden_dim: int | None = None, multiple_of: int = 256)->None:
+        super().__init__()
+
+        if hidden_dim is None:
+            hidden_dim = int((8 * d_in) / 3)
+
+        hidden_dim = multiple_of * math.ceil(hidden_dim / multiple_of)
+
+        self.w1 = nn.Linear(d_in, hidden_dim, bias=False)
+        self.v = nn.Linear(d_in, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, d_in, bias=False)
+
+    def forward(self, x: torch.Tensor)->torch.Tensor:
+        swish_gate = F.silu(self.w1(x))
+        value = self.v(x)
+        gated = swish_gate * value
+        output = self.w2(gated)
+
+        return output
